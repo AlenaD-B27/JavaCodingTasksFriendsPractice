f6110d67f4c4ea655517a6f696d88b6029b293ec
Zhypargul <nyc777kg@gmail.com>
2022-09-21 17:47:28 -0500

    FINRa is ready , created

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,51 @@ public class Main {
 
         OddAndEven OddEven = new OddAndEven();
         System.out.println(OddEven.OddOrEven(55));
+
+         FinraMethod();
+
+
+
     }
-}
+    public static void FinraMethod() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
