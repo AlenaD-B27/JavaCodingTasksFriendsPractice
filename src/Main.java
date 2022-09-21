84b550967789f6d44ab0d1bc527e1b3cf25c258b
Zhypargul <nyc777kg@gmail.com>
2022-09-21 16:52:50 -0500

    Odd and Even Task created and done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        OddAndEven OddEven = new OddAndEven();
+        System.out.println(OddEven.OddOrEven(55));
     }
 }
