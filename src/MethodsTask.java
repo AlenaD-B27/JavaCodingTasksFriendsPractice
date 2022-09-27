608aae21b41c1b5d9a16b2b4e8dd2d5e9492f6fa
olhasoroka <olly0296@gmail.com>
2022-09-26 19:07:04 -0500

    Even or Odd method is created.

---
+++ src/MethodsTask.java
@@ -0,0 +1,36 @@
+public class MethodsTask {
+
+    public static void evenOrOdd(int num) {
+
+        if (num % 2 == 0) {
+            System.out.println("This is an even number");
+        } else {
+            System.out.println("This is an odd number");
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    evenOrOdd(2);
+
+
+    }
+}
