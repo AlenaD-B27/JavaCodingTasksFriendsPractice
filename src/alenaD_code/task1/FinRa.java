da9fda1d6d3beff4e201c0b59209d7ceb42b6071
AlenaD-B27 <lenador.18@gmail.com>
2022-09-30 00:04:07 -0500

    Optimised Finra.java (still needs extra method).

--- src/alenaD_code/task1/FinRa.java
+++ src/alenaD_code/task1/FinRa.java
@@ -5,25 +5,44 @@ public class FinRa {
     Write a method which prints out the numbers from 1 to 30 but for numbers which are a multiple of 3 print "FIN" instead of the number and for numbers which are a multiple of 5, print "RA" instead of the number. for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
      */
 
-    public static void finra(){
-        for(int i = 1; i <= 30; i++){
+    public static void finra(int first, int last) {
+        if (first < last) {
 
-            if(i % 3 == 0 && i % 5 == 0){
-                System.out.print("FINRA ");
-            } else if (i % 3 == 0) {
-                System.out.print("FIN ");
-            } else if (i % 5 == 0) {
-                System.out.print("RA ");
-            } else {
-                System.out.print(i + " ");
-            }
+            for (int i = first; i <= last; i++) {
 
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.print("FINRA ");
+                } else if (i % 3 == 0) {
+                    System.out.print("FIN ");
+                } else if (i % 5 == 0) {
+                    System.out.print("RA ");
+                } else {
+                    System.out.print(i + " ");
+                }
 
+            } // end of loop
+            System.out.println();
+        } else {
+            for (int i = last; i <= first; i++) {
+
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.print("FINRA ");
+                } else if (i % 3 == 0) {
+                    System.out.print("FIN ");
+                } else if (i % 5 == 0) {
+                    System.out.print("RA ");
+                } else {
+                    System.out.print(i + " ");
+                }
+            } // end of loop
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        finra();
+        finra(-20,5);
+        finra(15,-15);
+        finra(5,5);
     }
 
 
