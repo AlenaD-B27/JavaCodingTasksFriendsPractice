82edcd83f7c68fb1ba66b2d76c71cec0fb3cb173
olhasoroka <olly0296@gmail.com>
2022-09-30 21:48:35 -0500

    finra method created

--- src/MethodsTask.java
+++ src/MethodsTask.java
@@ -27,14 +27,15 @@ public class MethodsTask {
     }
 
     public static void finra(){
-        for (int i = 1; i <= 30; i++) {
-            System.out.println(i);
+        for (int i = 1; i <= 30; i++) {  // checking numbers from 1 to 30
             if (i % 3 == 0 && i % 5 == 0){
                 System.out.println("FINRA");
             } else if (i % 5 == 0) {
                 System.out.println("RA");
             } else if (i % 3 == 0) {
                 System.out.println("FIN");
+            }else{
+                System.out.println(i);
             }
         }
     }
@@ -42,8 +43,8 @@ public class MethodsTask {
     public static void main(String[] args) {
 
    // evenOrOdd(2);
-    withoutDivideOperator(6,2);
-  //  finra();
+   // withoutDivideOperator(6,2);
+    finra();
 
     }
 }
