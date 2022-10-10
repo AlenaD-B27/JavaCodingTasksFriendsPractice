13f4c4631f0e09d2fed2fd800c6a1657ae0a91e2
AlenaD-B27 <lenador.18@gmail.com>
2022-10-10 01:34:52 -0500

    ArmstrongNumber.java is done, but I need to review tomorrow again

---
+++ src/alenaD_code/task3/ArmstrongNumber.java
@@ -0,0 +1,57 @@
+package alenaD_code.task3;
+
+public class ArmstrongNumber {
+    //Write a method that can check if a number is Armstrong number.
+    /* Rules of an Armstrong number:
+
+    --- is the sum of its own digits each raised to the power of the number of digits.
+    --- can NOT be a negative number.
+
+     */
+
+    public static void checkIfArmstrong(int num){
+
+
+
+        int totalDigits = 0;
+        for(int i = num; i != 0 ; i /= 10){
+            totalDigits++;
+        }
+
+        int [] digitsInPower = new int [totalDigits];
+
+        double dummyDouble = num;
+        int dummyInt = num;
+
+        for(int i = 0; i < totalDigits; i++){
+            dummyDouble /= 10 ;
+            dummyInt /= 10;
+            digitsInPower[i] = (int) Math.pow((dummyDouble-dummyInt)*10, totalDigits);
+            dummyDouble -= (dummyDouble-dummyInt);
+        }
+
+        int sumOfPoweredDigits = 0;
+
+        for(int i = 0; i < digitsInPower.length; i++){
+            sumOfPoweredDigits += digitsInPower[i];
+        }
+
+
+        System.out.println(num + (num == sumOfPoweredDigits ? " is an armstrong number" : " is not an armstrong number"));
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        checkIfArmstrong(153);
+    }
+
+
+
+
+
+
+
+}
