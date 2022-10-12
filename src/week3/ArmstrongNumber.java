fe01e5ba83a9eb4bc568fc7571835a6c106371b3
Mariia <kitmaria.2015@gmail.com>
2022-10-12 14:54:16 +0200

    isArmstrongNumber function created

---
+++ src/week3/ArmstrongNumber.java
@@ -0,0 +1,24 @@
+package week3;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        System.out.println("isArmstrongNumber(153) = " + isArmstrongNumber(153));
+        System.out.println("isArmstrongNumber(123) = " + isArmstrongNumber(123));
+    }
+    public static boolean isArmstrongNumber (int number){
+
+        // we will be taking only 3 digits
+        int sumOfNumber, lastDigit,secondDigit,firstDigit;
+        lastDigit = number % 10; // we will get the reminder ex // 123%10 = 3 there are 12 times 10 repeating
+
+        secondDigit =  (number/10) % 10;// its int so we can get reminder in decimal: we need to divide by 10 to get 2 first digits when we will be divided by 10 we will get 2 first numbers, but we need just second so we have to use % 10
+        firstDigit = number/100;
+        sumOfNumber = (firstDigit*firstDigit*firstDigit)+(secondDigit*secondDigit*secondDigit)+(lastDigit*lastDigit*lastDigit);
+        if(sumOfNumber == number){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+}
