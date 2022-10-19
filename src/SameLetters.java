ec431e2335883dd47b356b381f6afa669f6d8733
olhasoroka <olly0296@gmail.com>
2022-10-19 03:12:08 -0500

    Same letters method is created

---
+++ src/SameLetters.java
@@ -0,0 +1,32 @@
+public class SameLetters {
+
+    /*
+    String -- Same letters
+Write a return method that checks if a string is built out of the same letters as another string.
+Ex: same("abc", "cab"); -> true
+same("abc", "abb"); -> false:
+     */
+
+    public static boolean sameLetters(String str1, String str2) {
+
+       String word = "";
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = 0; j < str2.length(); j++) {
+
+                if (str1.charAt(i) == str2.charAt(j)){
+                    word += str2.charAt(j);
+                }
+            }
+        }
+        if(word.equals(str1)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sameLetters("abc","aab"));
+    }
+}
+
